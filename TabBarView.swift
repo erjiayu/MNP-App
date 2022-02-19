c3b9fdac1b061f610c41d9025b903cecd224fdd1
kxlee20 <73764377+kxlee20@users.noreply.github.com>
2022-02-19 16:34:26 -0500

    Commented out, null file

--- TabBarView.swift
+++ TabBarView.swift
@@ -1,75 +1,75 @@
-//
-//  TabBarView.swift
-//  Armapp
-//
-//  Created by Katherine Lee on 1/1/22.
-//
+// //
+// //  TabBarView.swift
+// //  Armapp
+// //
+// //  Created by Katherine Lee on 1/1/22.
+// //
 
-import SwiftUI
+// import SwiftUI
 
-struct TabBarView: View {
-    @Binding var selection: Int
-    @Namespace private var currentTab
+// struct TabBarView: View {
+//     @Binding var selection: Int
+//     @Namespace private var currentTab
     
     
-    var body: some View {
+//     var body: some View {
         
-        HStack(alignment: .bottom) {
-            ForEach(tabs.indices) {index in
-                GeometryReader { geometry in
-                    VStack(spacing: 4) {
-                        if selection == index {
-                            Color(.label)
-                                .frame(height: 2)
-                                .offset(y: -8)
-                                .matchedGeometryEffect(id: "currentTab", in: currentTab)
-                        }
+//         HStack(alignment: .bottom) {
+//             ForEach(tabs.indices) {index in
+//                 GeometryReader { geometry in
+//                     VStack(spacing: 4) {
+//                         if selection == index {
+//                             Color(.label)
+//                                 .frame(height: 2)
+//                                 .offset(y: -8)
+//                                 .matchedGeometryEffect(id: "currentTab", in: currentTab)
+//                         }
                         
-                        if tabs[selection].label == "Analytics" &&
-                            tabs[index].label == "Analytics" {
-                            Image(systemName: tabs[index].image)
-                                .frame(height: 20)
-                                .rotationEffect(.degrees(25))
-                        } else {
-                            Image(systemName: tabs[index].image)
-                                .frame(height: 20)
-                                .rotationEffect(.degrees(0))
-                        }
+//                         if tabs[selection].label == "Analytics" &&
+//                             tabs[index].label == "Analytics" {
+//                             Image(systemName: tabs[index].image)
+//                                 .frame(height: 20)
+//                                 .rotationEffect(.degrees(25))
+//                         } else {
+//                             Image(systemName: tabs[index].image)
+//                                 .frame(height: 20)
+//                                 .rotationEffect(.degrees(0))
+//                         }
                         
-                        Text(tabs[index].label)
-                            .font(.caption2)
-                            .fixedSize()
-                    }
-                    .fixedSize(horizontal: false, vertical: true)
-                    .frame(width: geometry.size.width / 2, height: 44, alignment: .bottom)
-                    .padding(.horizontal)
-                    .foregroundColor(selection == index ? Color(.orange) : .black)
+//                         Text(tabs[index].label)
+//                             .font(.caption2)
+//                             .fixedSize()
+//                     }
+//                     .fixedSize(horizontal: false, vertical: true)
+//                     .frame(width: geometry.size.width / 2, height: 44, alignment: .bottom)
+//                     .padding(.horizontal)
+//                     .foregroundColor(selection == index ? Color(.orange) : .black)
                     
-                    //adding functionality
-                    .onTapGesture {
-                        withAnimation {
-                            selection = index
-                        }
-                    }
-                }
-                .frame(height: 44, alignment: .bottom)
-            }
-        }
-    }
-}
+//                     //adding functionality
+//                     .onTapGesture {
+//                         withAnimation {
+//                             selection = index
+//                         }
+//                     }
+//                 }
+//                 .frame(height: 44, alignment: .bottom)
+//             }
+//         }
+//     }
+// }
 
 
-struct TabBarView_Previews: PreviewProvider {
-    static var previews: some View {
-        TabBarView(selection: Binding.constant(0))
-            .previewLayout(.sizeThatFits)
-    }
-}
+// struct TabBarView_Previews: PreviewProvider {
+//     static var previews: some View {
+//         TabBarView(selection: Binding.constant(0))
+//             .previewLayout(.sizeThatFits)
+//     }
+// }
 
-let tabs = [
-    TabStruct(image: "house.fill", label: "Home"),
-    TabStruct(image: "person.fill", label: "User"),
-    TabStruct(image: "mic.fill", label: "Microphone"),
-    TabStruct(image: "slider.vertical.3", label: "Settings"),
-    TabStruct(image: "gear", label: "Analytics")
-]
+// let tabs = [
+//     TabStruct(image: "house.fill", label: "Home"),
+//     TabStruct(image: "person.fill", label: "User"),
+//     TabStruct(image: "mic.fill", label: "Microphone"),
+//     TabStruct(image: "slider.vertical.3", label: "Settings"),
+//     TabStruct(image: "gear", label: "Analytics")
+// ]
