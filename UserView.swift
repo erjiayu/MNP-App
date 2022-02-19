0302355570b0518c702495b547071622b021763c
erjiayu <erjiayu@umich.edu>
2022-02-19 14:09:00 -0500

    Added basic User page.

--- UserView.swift
+++ UserView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct UserView: View {
-    var body: some View {
+    let profileLinkNames: [String] = ["Friends", "Tips", "Settings"]
         
-        Color.blue.ignoresSafeArea(edges: .top)
+    var body: some View {
+        NavigationView {
+            VStack {
+                ForEach(profileLinkNames, id: \.self) { profileLinkName in
+                UserLink(profileLinkName: profileLinkName)
+            }
+            Spacer()
+        }
+        .navigationBarTitle("Tara Yu")
     }
 }
 
 struct UserView_Previews: PreviewProvider {
     static var previews: some View {
         UserView()
+    }
+}
+
+struct UserLink: View {
+    let profileLinkName: String     // Add parameter for profileLinkName
+    
+    var body: some View {
+        VStack {
+                HStack {
+                    Text(profileLinkName)
+                        .font(.body)
+                    Spacer()
+                    Image(systemName: "chevron.right")
+                        .foregroundColor(Color(.blue))
+                }
+                .padding(EdgeInsets(top: 10, leading: 15, bottom: 10, trailing: 15))
+                Divider()
+            }
+        }
+        
     }
 }
